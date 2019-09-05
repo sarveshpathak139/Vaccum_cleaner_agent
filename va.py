5e15d410fe35cfcd14cd8d271308ee3463b46c26
sarveshpathak139 <42840561+sarveshpathak139@users.noreply.github.com>
2019-09-05 10:03:50 +0530

    Add files via upload

---
+++ va.py
@@ -0,0 +1,14 @@
+
+print("----------------------------------------------------------")
+print("*****************Vaccum Cleaner Agent*********************")
+print("----------------------------------------------------------")
+room1 = ["Wet garbage","Dry garbage","Fruits"]
+print("Garbage in room1 is",room1)
+print("***********************************************************")
+room2 = ["Vegetables","Roti","Salad"]
+print("Garbage in room2 is",room2)
+print("***********************************************************")
+vaccuma= room1 + room2
+print("Garbage collected is",vaccuma)
+print("***********************************************************")
+
